673f3df30fbc9ac3e2a4376d2c95b3dda924db73
Yegorov <yegorov0725@yandex.ru>
2016-11-01 17:10:42 +0400

    Some fix

--- models/index.js
+++ models/index.js
@@ -8,17 +8,19 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  if (env === 'production') {
-    if(!process.env.MYSQL_CONNECTION_STRING) 
-        throw new Error("Env variable MYSQL_CONNECTION_STRING not define");
-    var sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING);
-  }
-  else
-     var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (env === 'production') { 
+  if(!process.env.MYSQL_CONNECTION_STRING) 
+      throw new Error("Env variable MYSQL_CONNECTION_STRING not define");
+  var sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING);
 }
+else if(env === 'development')  {
+  if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
+}
+
 
 
 fs
